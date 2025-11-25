1fa7b300d68f22910098d2b0f333d0fe893a3893
pmshrik <142974574+pmshrik@users.noreply.github.com>
2025-11-25 12:07:47 +0530

    Update dockerPush.groovy

--- vars/dockerPush.groovy
+++ vars/dockerPush.groovy
@@ -1,10 +1,10 @@
 def call() {
-  
+  withCredentials([usernamePassword(credentialsId: 'dockerHubCred', usernameVariable: 'USER', passwordVariable: 'PASS')]) {
     sh """
-      
+      echo $PASS | docker login -u $USER --password-stdin
       docker tag cicd_django_notes_app-django_app:latest $USER/django-notes-app:latest
       docker push $USER/django-notes-app:latest
     """
-  
+  }
 }
 
