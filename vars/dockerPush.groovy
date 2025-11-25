114087bfadd2b2361194a9b28c3ac702f297e0ec
pmshrik <142974574+pmshrik@users.noreply.github.com>
2025-11-25 11:16:32 +0530

    Update dockerPush.groovy

--- vars/dockerPush.groovy
+++ vars/dockerPush.groovy
@@ -1,7 +1,8 @@
-def call() {
+def call(user) {
   sh """
     docker tag cicd_django_notes_app-django_app:latest ${user}/django-notes-app:latest
     docker push ${user}/django-notes-app:latest
   """
 }
+
 
