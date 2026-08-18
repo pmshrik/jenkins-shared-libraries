fbd7fad81bd5e2e5fc21ffc1654ec2dae428b377
pmshrik <142974574+pmshrik@users.noreply.github.com>
2026-08-18 09:26:35 +0530

    Remove 'noupdate' argument from dependency check

--- vars/owasp_dependency.groovy
+++ vars/owasp_dependency.groovy
@@ -1,6 +1,4 @@
 def call(){
-    dependencyCheck additionalArguments: '--scan ./ --noupdate',
-                     odcInstallation: 'OWASP'
-
-    dependencyCheckPublisher pattern: '**/dependency-check-report.xml'
+  dependencyCheck additionalArguments: '--scan ./', odcInstallation: 'OWASP'
+  dependencyCheckPublisher pattern: '**/dependency-check-report.xml'
 }
